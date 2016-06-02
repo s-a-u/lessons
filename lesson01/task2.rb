2d6675e9f97017fc3d3937b75e04cfeb6ffbd38b
s-a-u <s-a-u@i.ua>
2016-06-02 22:31:14 +0300

    change 	modified:   task2.rb 	modified:   task3.rb

--- lesson01/task2.rb
+++ lesson01/task2.rb
@@ -1,7 +1,7 @@
 puts "Введите основание треугольника"
-bottom = gets
+bottom = gets.chomp
 
 puts "Введите высоту треугольника"
-height = gets
+height = gets.chomp
 
 puts "Площадь треугольника #{bottom.to_i * height.to_i / 2.0}"
