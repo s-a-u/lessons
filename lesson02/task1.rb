f97e5272fa7eb6a80486066ea741cabe71fb5f33
s-a-u <s-a-u@i.ua>
2016-06-03 22:32:54 +0300

    change 	new file:   task2.rb

---
+++ lesson02/task1.rb
@@ -0,0 +1,5 @@
+puts "you name"
+months = {:"January"=>31, :"Febriary"=>28, :"March"=>31,
+:"April"=>30, :"May"=>31, :"June"=>30, :"July"=>31, :"August"=>30, 
+:"September"=>31, :"October"=>30, :"November"=>31,:"Desember"=>30}
+puts months[30]
