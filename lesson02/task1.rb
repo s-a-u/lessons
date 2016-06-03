26279fc273077fad67eb7f22bc1e44cde534e059
s-a-u <s-a-u@i.ua>
2016-06-04 00:45:28 +0300

    change 	modified:   task1.rb

--- lesson02/task1.rb
+++ lesson02/task1.rb
@@ -1,5 +1,6 @@
-puts "you name"
+
 months = {:"January"=>31, :"Febriary"=>28, :"March"=>31,
-:"April"=>30, :"May"=>31, :"June"=>30, :"July"=>31, :"August"=>30, 
-:"September"=>31, :"October"=>30, :"November"=>31,:"Desember"=>30}
-puts months[30]
+:"April"=>30, :"May"=>31, :"June"=>30, :"July"=>31, :"August"=>31, 
+:"September"=>30, :"October"=>31, :"November"=>30,:"Desember"=>31}
+
+puts months.select {|k,v| v ==30}
