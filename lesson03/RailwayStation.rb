6873bf4ce6c313fa4d447e1bfb1db46c821ada6e
s-a-u <s-a-u@i.ua>
2016-06-13 14:52:29 +0300

    change 	modified:   RailwayStation.rb

--- lesson03/RailwayStation.rb
+++ lesson03/RailwayStation.rb
@@ -1,20 +1,30 @@
 class RailwayStation
-def name_staion
-name
-end
-def get_one_train
+	
+	attr_accessor :stations
+	attr_accessor :route
+    attr_accessor :train
 
-end
-def show_all_trains_this_moment
-  if departure_one_train
-  	delete_from_show list
-  end	
+	def       initialize(stations=[],train=[])
+	 	@stations=[],
+	 	@train=[],
+	 	@route
+	 	end
+
+def get_one_train
+	@route.stations[@station_index].add_train(self)
+@train_number
 end
 def show_type_all_trains
 	cargo /passenger
+	puts @type.select(cargo)
+	puts @type.select(passenger)
 end
 def departure_one_train
 	departure_one_train
+	@route.stations[@station_index].delete_train(self)
 end	
-
+def show_all_trains_this_moment
+  if departure_one_train
+  end	
+   puts @trains
 end	
